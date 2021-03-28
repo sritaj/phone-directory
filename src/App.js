716ada964710cf75adb52c0de0a585b0c1dfe6b0
Sritaj <sritajp@gmail.com>
2021-03-29 01:04:52 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-
-
 function App() {
   return (
-    <div >
-      
+    <div>
+      {/*<div className="header">Phone Directory</div>
+      <button>Add</button>
+      <div>
+        <span>Name</span>
+        <br />
+        <span>Phone no</span>
+  </div> */}
+      <label htmlFor="name">Name:</label>
+      <input
+        id="name"
+        type="text"
+        placeholder="Type Here"
+        defaultValue="sritaj"
+      ></input>
     </div>
   );
 }
