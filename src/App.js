50adf301afd90dedc6c110e990de48686f3cd8bf
Sritaj <sritajp@gmail.com>
2021-03-29 22:27:11 +0530

    Added Header in Header.JS file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
+import Header from "./Header";
+
 function App() {
   return (
     <div>
-      {/*<div className="header">Phone Directory</div>
+      <Header />
       <button>Add</button>
+
       <div>
-        <span>Name</span>
-        <br />
-        <span>Phone no</span>
-  </div> */}
-      <label htmlFor="name">Name:</label>
-      <input
-        id="name"
-        type="text"
-        placeholder="Type Here"
-        defaultValue="sritaj"
-      ></input>
+        <span>Name</span> <br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = function () {
+  return <div className="header">Phone Directory</div>;
+};
+
+export default Header;
