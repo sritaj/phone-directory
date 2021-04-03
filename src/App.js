eb28711c953042f602eb258fd82c4f45fa34aead
Sritaj <sritajp@gmail.com>
2021-04-04 00:12:24 +0530

    Added External Styling for Header, Added CSS for ADD button, Name and Phone, Added Delete Button, Added Props for dynamically changing header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import Header from "./Header";
+import "./App.css";
+import "./Header.css";
 
 function App() {
+  let subscribers = [
+    {
+      id: 1,
+      name: "Sritaj",
+      phone: "8399393933",
+    },
+    {
+      id: 2,
+      name: "test",
+      phone: "8494494949",
+    },
+  ];
   return (
     <div>
-      <Header />
-      <button>Add</button>
+      <Header heading="Phone Directory" />
+      <div className="component-body-container">
+        <button className="custom-btn add-btn">Add</button>
 
-      <div>
-        <span>Name</span> <br />
-        <span>Phone</span>
+        <div className="grid-container heading-container">
+          <span className="grid-item name-heading">Name</span>
+          <span className="grid-item phone-heading">Phone</span>
+        </div>
+
+        {subscribers.map((sub) => {
+          return (
+            <div key={sub.id} className="grid-container heading-container">
+              <span className="grid-item name-heading">{sub.name}</span>
+              <span className="grid-item phone-heading">{sub.phone}</span>
+              <span className="grid-item action-btn-container">
+                <button className="custom-btn delete-btn">Delete</button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,8 @@
 import React from "react";
+import "./Header.css";
 
-const headerStyle = {
-  textAlign: "center",
-  padding: 20,
-  background: "#000",
-  color: "#fff",
-  textTransform: "uppercase",
-};
-
-const Header = function () {
-  return <div style={headerStyle}>Phone Directory</div>;
+const Header = function (props) {
+  return <div className="header">{props.heading}</div>;
 };
 
 export default Header;
